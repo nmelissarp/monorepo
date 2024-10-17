75057149da23f3eb6391755e71324d59668d5c12
nmelissarp <nmelissarp077@gmail.com>
2024-10-17 11:43:47 -0600

    feat: background blue

--- shared-components/src/HolaMundo.js
+++ shared-components/src/HolaMundo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HolaMundo = ({ appName }) => {
   return (
-    <h1 style={{ backgroundColor: 'red', color: 'white' }}>
+    <h1 style={{ backgroundColor: 'blue', color: 'white' }}>
       Hello from {appName}!!
     </h1>
   )
